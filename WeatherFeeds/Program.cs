36516a232b6b2d994cf5c48d792123f169d2c17a
ses1989 <37503617+ses1989@users.noreply.github.com>
2021-02-04 19:35:14 +0530

    Initial Commit of Project

---
+++ WeatherFeeds/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WeatherFeeds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
